3264ad9923b57dcb839462abe60a313616e5643c
zhangsan <zhangsan@itcast.cn>
2019-06-24 20:17:47 +0800

    输出了spring家族信息

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("spring-mvc!");
+        System.out.println("spring-boot!");
+        System.out.println("spring-cloud!");
     }
 }
